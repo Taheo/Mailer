1544cc118b402fbcae266dd696744757ac2eed31
Michał Segieta <segieta.michal@gmail.com>
2017-01-08 21:07:04 +0100

    code cleaning

--- Mailer/Program.cs
+++ Mailer/Program.cs
@@ -12,21 +12,16 @@ namespace Mailer
 {
     public class Mailer
     {
-        public Mailer()
-        {
-
-        }
+        public Mailer(){}
 
         public List<string> emails = new List<string>();
         public List<string> logs = new List<string>();
         public void ReadCSV()
         {
             try
-            {   // Open the text file using a stream reader.
-
+            {   
                 using (StreamReader sr = new StreamReader("emails.csv"))
                 {
-                    // Read the stream to a string, and write the string to the console.
                     while (!sr.EndOfStream)
                     {
                         emails.Add(sr.ReadLine());
@@ -39,70 +34,42 @@ namespace Mailer
                 Console.WriteLine(ex.Message);
             }
         }
+        //private void ReadTXT()
+        //{
+        //    try
+        //    {   // Open the text file using a stream reader.
 
-        private void ReadTXT()
-        {
-            try
-            {   // Open the text file using a stream reader.
+        //        using (StreamReader sr = new StreamReader("logi.txt"))
+        //        {
+        //            // Read the stream to a string, and write the string to the console.
+        //            while (!sr.EndOfStream)
+        //            {
+        //                logs.Add(sr.ReadLine());
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine("The file could not be read:");
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //}
 
-                using (StreamReader sr = new StreamReader("logi.txt"))
-                {
-                    // Read the stream to a string, and write the string to the console.
-                    while (!sr.EndOfStream)
-                    {
-                        logs.Add(sr.ReadLine());
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine("The file could not be read:");
-                Console.WriteLine(ex.Message);
-            }
-        }
-
-        void SaveLogs()
-        {
-            try
-            {
-                using (StreamWriter outputFile = new StreamWriter("logi.txt"))
-                {
-                    foreach (string item in logs)
-                        outputFile.WriteLine(item);
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
-
-        public string FindNew()
-        {
-            bool check;
-
-            foreach (var item1 in emails)
-            {
-                check = false;
-                foreach (var item2 in logs)
-                {
-
-                    if (item1 == item2)
-                    {
-                        check = true;
-                        break;
-                    }
-
-                }
-                if (check == false)
-                {
-                    return item1;
-                }
-            }
-            return null;
-        }
-        
-
+        //void SaveLogs()
+        //{
+        //    try
+        //    {
+        //        using (StreamWriter outputFile = new StreamWriter("logi.txt"))
+        //        {
+        //            foreach (string item in logs)
+        //                outputFile.WriteLine(item);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //}
         public void SendAll(List<string> emails, List<string> logi)
         {
             using (MailMessage mail = new MailMessage())
